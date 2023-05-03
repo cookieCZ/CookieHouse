cb9c0ad54519f84c82ddbbe9b2b920582719d9f4
cookieCZ <martin.hlusek@email.cz>
2023-05-03 17:29:06 +0200

    3. 5.

--- pec.gd
+++ pec.gd
@@ -3,25 +3,45 @@ extends Area2D
 var cookie = preload("res://cookie.tscn")
 var pasSvisly = preload("res://pasSvisly.tscn")
 
-var spawnZaMinutu = 12
+var spawnZaMinutu
 var cenaSusenky
 var upgradeRychlosti
 var upgradeCeny
 var smerSusenky
-var koupena = false
+var koupena
 var cenaPece
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	nastav_smer_spawnu()
+	pass
+
+func vytvor_pec(cisloPece):
 	$Button.modulate = Color("#FFFFFFFF")
 	$Button.text = "+: " + Cisla.zobraz(cenaPece)
+	true_init(
+		pow(1000, cisloPece),
+		ceil(pow(1000, cisloPece) / 2.0),
+		pow(100, cisloPece) * 20,
+		pow(100, cisloPece) * 20,
+		12, false)
 
-func muj_init(cisloPece):
-	cenaPece = pow(1000, cisloPece)
-	cenaSusenky = pow(100, cisloPece) * 5
-	upgradeRychlosti = pow(100, cisloPece) * 20
-	upgradeCeny = pow(100, cisloPece) * 20
+#func nacti_pec(cenaSusenky, upgradeRychlosti, upgradeCeny, spawnZaMinutu):
+#	true_init(0, cenaSusenky, upgradeRychlosti, upgradeCeny, spawnZaMinutu, true)
+func nacti_pec(cena_susenky, upgrade_rychlosti, upgrade_ceny, spawn_za_minutu):
+	vytvor_pas()
+	true_init(0, cena_susenky, upgrade_rychlosti, upgrade_ceny, spawn_za_minutu, true)
+
+func true_init(cena_pece, cena_susenky, upgrade_rychlosti, upgrade_ceny, spawn_za_minutu, je_koupena):
+	cenaPece = cena_pece
+	cenaSusenky = cena_susenky
+	upgradeRychlosti = upgrade_rychlosti
+	upgradeCeny = upgrade_ceny
+	spawnZaMinutu = spawn_za_minutu
+	koupena = je_koupena
+	if(position.y<get_viewport_rect().size.y/2):
+		smerSusenky = "dolů"
+	else:
+		smerSusenky = "nahoru"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -32,6 +52,7 @@ func vytvor_pas():
 	var y = position.y
 	if(smerSusenky == "dolů"):
 		y += get_viewport_rect().size.y/4
+		pas.scale *= -1
 	else:
 		y -= get_viewport_rect().size.y/4
 	pas.position = Vector2(position.x, y)
@@ -39,7 +60,7 @@ func vytvor_pas():
 	get_parent().add_child(pas)
 
 func spawn():
-	if(koupena):
+	if koupena:
 		var susenka = cookie.instantiate()
 		susenka.nastav_cenu(cenaSusenky)
 		susenka.nastav_smer(smerSusenky)
@@ -50,15 +71,8 @@ func spawn():
 func _on_timer_timeout():
 	spawn()
 
-func nastav_smer_spawnu():
-	if(position.y<get_viewport_rect().size.y/2):
-		smerSusenky = "dolů"
-	else:
-		smerSusenky = "nahoru"
-
-
 func _on_button_pressed():
-	if(koupena):
+	if koupena:
 		$MarginContainer/VBoxContainer/CenaLabel.text = Cisla.zobraz(cenaSusenky) + "/ks"
 		$MarginContainer/VBoxContainer/RychlostLabel.text = Cisla.zobraz(spawnZaMinutu) + "/min"
 		$MarginContainer/VBoxContainer/CenaButton.text = Cisla.zobraz(upgradeCeny)
@@ -78,7 +92,6 @@ func _on_cena_button_pressed():
 		upgradeCeny *= 2
 		cenaSusenky = ceil(cenaSusenky * 1.1)
 	$MarginContainer.visible = false
-
 
 func _on_rychlost_button_pressed():
 	if(get_parent().penize >= upgradeRychlosti):
--- svet.gd
+++ svet.gd
@@ -1,24 +1,50 @@
 extends Node2D
 
-var penize = 5
+var penize
 
 var pecPreload = preload("res://pec.tscn")
 
+var saveFile = "res://save.csv"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(6):
-		var pec = pecPreload.instantiate()
-		pec.muj_init(i)
-		var x = 880 - (i / 2) * 350
-		var y
-		if(i % 2 == 0):
-			y = 200
-		else:
-			y = 2000
-		pec.position = Vector2(x, y)
-		pec.z_index = 11
-		add_child(pec)
+	if FileAccess.file_exists(saveFile):
+		#jestli je save
+		#podobný, ale načítá z filosu saveFile
+		penize = 5
+		for i in range(6):
+			var pec = pecPreload.instantiate()
+			pec.vytvor_pec(i)	#používá nacti_pec()
+			var x = 880 - (i / 2) * 350
+			var y
+			if(i % 2 == 0):
+				y = 200
+			else:
+				y = 2000
+			pec.position = Vector2(x, y)
+			pec.z_index = 11
+			add_child(pec)
+	else:
+		#jestli není save
+		penize = 5
+		for i in range(6):
+			var pec = pecPreload.instantiate()
+			pec.vytvor_pec(i)
+			var x = 880 - (i / 2) * 350
+			var y
+			if(i % 2 == 0):
+				y = 200
+			else:
+				y = 2000
+			pec.position = Vector2(x, y)
+			pec.z_index = 11
+			add_child(pec)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$Label.text = Cisla.zobraz(penize)
+
+func _notification(what):
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		#ulož pece do filosu saveFile
+		get_tree().quit()
